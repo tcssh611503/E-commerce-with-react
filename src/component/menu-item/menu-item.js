cc2635a19c6a2e27d891e32e8838e81c783d2e34
york <york@ruten.co.jp>
2021-01-06 17:33:53 +0800

    update Google font and Menu css  style

--- src/component/menu-item/menu-item.js
+++ src/component/menu-item/menu-item.js
@@ -4,12 +4,15 @@ import  './menu-item.scss';
 //使用{} 
 //style={{ backgroundImage: `url(${imageUrl})` }} 串接CSS
 //增加size ${size}  class 給 className='menu-item'>
+// 從 className={`${size} menu-item `} 改到 style={{ backgroundImage: `url(${imageUrl})` }}
+//  <div   style={{ backgroundImage: `url(${imageUrl})` }} className={`${size} menu-item `} >
+
 const MenuItem = ({title , imageUrl , size}) => (
-  <div  className={`${size} menu-item `} >
+  <div className={`${size} menu-item `} >
     <div style={{ backgroundImage: `url(${imageUrl})` }}
        className='background-image'/>
     <div className='content'>
-      <h1 className='title'>{title}</h1>
+      <h1 className='title'>{title.toUpperCase}</h1>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
