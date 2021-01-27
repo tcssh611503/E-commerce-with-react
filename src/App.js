8dcdd6cbcb28334a79a955f8993e738107c75ba8
york <york@ruten.co.jp>
2021-01-27 16:10:00 +0800

    update build

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import './App.css';
-// import React, { Component } from 'react'
-// import { Switch , Route , Link } from "react-router-dom";
-import React from 'react'
-import { Switch , Route  } from "react-router-dom";
+import React, { Component } from 'react'
+import { Switch , Route , Link } from "react-router-dom";
+// import React from 'react'
+// import { Switch , Route  } from "react-router-dom";
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -30,6 +30,7 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+     <h1>HATS PAGE </h1>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/hats' component={HatsPage} />
